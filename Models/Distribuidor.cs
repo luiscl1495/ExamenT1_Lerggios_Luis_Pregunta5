91a869b3ae40122e9c27facb2bdd809c4d7a2a4f
luiscl1495 <luis141495@gmail.com>
2026-06-21 01:52:36 +0200

    Second Commit

---
+++ Models/Distribuidor.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+
+namespace T1_Lerggios_Luis.Models
+{
+    public class Distribuidor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El Nombre del distribuidor es obligatorio")]
+        public string NombreDistribuidor { get; set; }
+
+        [Required(ErrorMessage = "La Razon Social del distribuidor es obligatoria")]
+        public string RazonSocial { get; set; }
+
+        [Required(ErrorMessage = "El Telefono del distribuidor es obligatorio")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "El Año de operación es obligatorio")]
+        [Range(1900, 3000, ErrorMessage = "El año de operación debe estar entre 1900 y 3000")]
+        public int AnioInicioOperacion { get; set; }
+
+        //[Required(ErrorMessage = "El contacto es obligatorio")]
+        public string Contacto { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace T1_Lerggios_Luis.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
